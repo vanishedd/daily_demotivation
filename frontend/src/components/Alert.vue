cf1a46bced30f7eadee8d06e5d6695bf0557fea5
Nik <101550927+vanishedd@users.noreply.github.com>
2024-02-19 09:05:00 +0100

    updated alert

---
+++ frontend/src/components/Alert.vue
@@ -0,0 +1,46 @@
+<!-- Alert.vue -->
+<template>
+  <transition name="fade">
+    <div v-if="isVisible" class="alert alert-error bottom-right">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="stroke-current shrink-0 h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      <span>Error! Task failed successfully.</span>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    isVisible: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.bottom-right {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+}
+</style>
