2e726785c37c07b98abcb29046866dbbbdf2860f
Nik <101550927+vanishedd@users.noreply.github.com>
2024-02-19 08:54:35 +0100

    deleted vercel analytics

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,10 @@
 import navbar from "./components/Navbar.vue";
 import daily from "./components/DailyQuote.vue";
 import footerVue from "./components/Footer.vue";
-import { Analytics } from "@vercel/analytics";
 </script>
 
 <template>
   <div class="flex flex-col h-screen">
-    <Analytics />
     <navbar />
     <daily />
     <footerVue />
